6140000614419a54a50f6725237afd9214550200
buzzkillb <travanx@gmail.com>
2019-01-14 20:58:18 -0800

    grab wallet

---
+++ grim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "get binary and untar"
+wget https://github.com/mimblewimble/grin/releases/download/v0.5.2/grin-v0.5.2-478131988-linux-amd64.tgz
+tar -xvf grin-v0.5.2-478131988-linux-amd64.tgz
+sudo mv grin /usr/local/bin/grin
+
+echo "Install Stuff"
+sudo apt-get -y install build-essential cmake git libgit2-dev clang libncurses5-dev libncursesw5-dev zlib1g-dev pkg-config libssl-dev llvm
+
